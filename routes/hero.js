e4280507691e4fbab5272c890a77983f978ee2ba
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2020-11-19 08:15:04 +0100

    FETCH ARRAY FOR MOVIES, DESTRUCT ARRAY

--- routes/hero.js
+++ routes/hero.js
@@ -3,13 +3,44 @@ const axios = require("axios");
 const router = express.Router();
 
 //get the hero information - using req.params to pass the randomised Id and axios to get data from database.
+//get species name and page
+//get movies names
+//get planets pages
 
 router.get("/:id", async (req, res) => {
   try {
     let hero = await axios.get(
       `https://swapi.dev/api/people/${req.params.id}/`
     );
+    //KEEP ONLY DATA FROM HERO:
     hero = hero.data;
+
+    //PASSING MOVIES FOR THE HERO.HBS
+    let movies = hero.films;
+
+    //CREATE ARRAY OF INDEXES OF MOVIES
+    //step1 - leave numbers only - to pass to it to the page.
+    let movieNumArr = movies.map((item) => item.substr(item.length - 2));
+    //step2 - fetch all of the movies from the from the swapi
+    let dataMovie = await axios.all(movies.map((item) => axios.get(item)));
+    //step 3 - remove all of the information except for the data from the array
+    dataMovie = dataMovie.map(({ data, ...rest }) => data);
+    dataMovie = dataMovie.map(
+      ({
+        opening_crawl,
+        characters,
+        starships,
+        planets,
+        vehicles,
+        species,
+        created,
+        edited,
+        url,
+        ...rest
+      }) => rest
+    );
+    console.log(dataMovie);
+
     //to pass the data for species - sometimes species does not exist, return information about it.
     const species = hero.species;
     if (species[0]) {
@@ -20,14 +51,12 @@ router.get("/:id", async (req, res) => {
       );
       speciesName = speciesName.data;
       speciesName = speciesName.name;
-      //to pass the data for movies
-      let movies = hero.films;
-
       res.render("hero/hero", {
         layout: "main",
         hero,
         speciesId,
         speciesName,
+        movieNumArr,
       });
     } else {
       res.render("hero/hero", {
